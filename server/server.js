56674bdeb389ff4434617d82db38fd243b9af4a0
Subha2707 <deysubahdip66@gmail.com>
2026-03-13 09:43:32 +0530

    Fix CORS and Render port configuration

--- server/server.js
+++ server/server.js
@@ -9,13 +9,27 @@ const historyRoutes = require("./routes/history");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://pixeditor-app.netlify.app"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 /* MongoDB Connection */
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+/*Test Route */
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API running 🚀" });
+});
 
 /* Auth Routes */
 app.use("/api/auth", authRoutes);
@@ -36,6 +50,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
